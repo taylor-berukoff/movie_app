6f54ab39599c8e302fd533e2cfd55f9da1bc6f4d
Michaela1084 <142283872+Michaela1084@users.noreply.github.com>
2024-06-05 20:00:14 -0700

    Added color and sized h2

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ function App(){
     <div>
       <div>
         <h1 className="text-red-700 text-8xl">something</h1>
-        <h2>Something else altogether</h2>
+          {/* Giving color to h2 tags and resized text */}
+        <h2 className='text-blue-300 text-5sml'>Something else altogether</h2>
       </div>
     </div>
     </>
