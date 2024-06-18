01508095c600f68683d642a4d1d7b2ed69fd7ea5
tsb1995 <taylorberukoff@gmail.com>
2024-06-17 18:39:48 -0700

    update app to use react dom router. Get basic routing working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import Movie_vault from "./pages/Movie_vault";
+import User_Profile from "./pages/User_Profile";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App(){
   return(
-    <>
-      <Movie_vault />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Movie_vault />} />
+          <Route path="user_profile" element={<User_Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<App />);
---
+++ src/pages/User_Profile.tsx
@@ -0,0 +1,13 @@
+function User_Profile() {
+
+    
+    
+    
+      return (
+        <div>
+          <h1>This is a User Page</h1>
+        </div>
+      )
+    }
+    
+export default User_Profile
